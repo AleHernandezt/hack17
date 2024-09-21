628abb77f23afd87ce59fed8530e74c249779447
Alehernandezt <j.alehernandezt@gmail.com>
2024-09-21 18:35:43 -0400

    table is not working but tomorrow i'll fix it or create it once again

--- src/app/Views/Admin/gestion-categoria/gestion-categoria.component.ts
+++ src/app/Views/Admin/gestion-categoria/gestion-categoria.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BtnComponent } from "../../../Shared/btn/btn.component";
+import { TableComponent } from "../../../Shared/table/table.component";
 
 @Component({
   selector: 'app-gestion-categoria',
   standalone: true,
-  imports: [BtnComponent],
+  imports: [BtnComponent, TableComponent],
   templateUrl: './gestion-categoria.component.html',
   styleUrl: './gestion-categoria.component.css'
 })
