cfb6988fb09b4bb83632c20d7da34bc10cc9c9e7
Alehernandezt <j.alehernandezt@gmail.com>
2024-09-21 15:02:04 -0400

    sidebar 90%

--- src/app/Views/Admin/dash-admin/dash-admin.component.ts
+++ src/app/Views/Admin/dash-admin/dash-admin.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { SidebarComponent } from "../../../Shared/sidebar/sidebar.component";
 import { Side2Component } from "../../../Shared/side2/side2.component";
 
 @Component({
   selector: 'app-dash-admin',
   standalone: true,
-  imports: [SidebarComponent, Side2Component],
+  imports: [Side2Component],
   templateUrl: './dash-admin.component.html',
   styleUrl: './dash-admin.component.css'
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Side2Component } from "./Shared/side2/side2.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Side2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
