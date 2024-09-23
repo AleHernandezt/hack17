8e94ad74aee0335ec5ad904ce04a3e623175e249
alehernandezt <j.alehernandezt@gmail.com>
2024-09-23 16:47:48 -0400

    added h2 component in dashboar-admin

--- src/app/Views/Admin/dash-admin/dash-admin.component.ts
+++ src/app/Views/Admin/dash-admin/dash-admin.component.ts
@@ -3,6 +3,7 @@ import { Side2Component } from "../../../Shared/side2/side2.component";
 import { H1Component } from '../../../Shared/h1/h1.component';
 import { H2Component } from "../../../Shared/h2/h2.component";
 
+
 @Component({
   selector: 'app-dash-admin',
   standalone: true,
