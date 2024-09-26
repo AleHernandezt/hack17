e93a2adbbe0a7de6cc2024747a90d85173eeef60
alehernandezt <j.alehernandezt@gmail.com>
2024-09-26 00:32:55 -0400

    working without data bc array(0) pero si conecta

--- src/app/Views/Admin/devoluciones/devoluciones.component.ts
+++ src/app/Views/Admin/devoluciones/devoluciones.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { H1Component } from "../../../Shared/h1/h1.component";
 import { BtnComponent } from "../../../Shared/btn/btn.component";
 import { SearchbarComponent } from "../../../Shared/searchbar/searchbar.component";
 import { Table2Component } from "../../../Shared/table2/table2.component";
+import { NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-devoluciones',
@@ -11,35 +12,29 @@ import { Table2Component } from "../../../Shared/table2/table2.component";
   templateUrl: './devoluciones.component.html',
   styleUrl: './devoluciones.component.css'
 })
-export default class DevolucionesComponent {
-  devoluciones = [
-    {
-      patient_id: 2,
-      treatment_id: 2,
-      appointment_date: new Date('2024-09-25'),
-      withdrawal_date: new Date('2024-09-26'),
-      expiration_date: new Date('2024-12-31'),
-      medications: [
-        {
-          medication_id: 1,
-          quantity: 2
+export default class DevolucionesComponent implements OnInit {
+  devoluciones: any[] = [];
+
+  constructor(private ngZone: NgZone) { }
+
+  ngOnInit(): void {
+    this.getDevoluciones();
+  }
+
+  getDevoluciones() {
+    fetch('http://localhost:3000/api/delivery/getDeleted')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json); // <--- Agrega esta línea para imprimir los datos en la consola
+        if (json && json.data && json.data.Delivery && Array.isArray(json.data.Delivery)) {
+          this.ngZone.run(() => {
+            this.devoluciones = json.data.Delivery;
+          });
+        } else {
+          console.error('La API no devolvió un arreglo de devoluciones');
         }
-      ]
-    },
-    {
-      patient_id: 3,
-      treatment_id: 3,
-      appointment_date: new Date('2024-10-01'),
-      withdrawal_date: new Date('2024-10-02'),
-      expiration_date: new Date('2024-12-31'),
-      medications: [
-        {
-          medication_id: 2,
-          quantity: 3
-        }
-      ]
-    }
-  ];
+      });
+  }
 
   editDevolucion(devolucion: any) {
     alert(devolucion.patient_id);
