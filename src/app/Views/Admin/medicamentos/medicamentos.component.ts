8757ed7d4a56e6e8e462cf822504a393d42feb25
alehernandezt <j.alehernandezt@gmail.com>
2024-09-26 00:48:36 -0400

    medicamentos done

--- src/app/Views/Admin/medicamentos/medicamentos.component.ts
+++ src/app/Views/Admin/medicamentos/medicamentos.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BtnComponent } from "../../../Shared/btn/btn.component";
 import { TableComponent } from "../../../Shared/table/table.component";
 import { H1Component } from '../../../Shared/h1/h1.component';
 import { SearchbarComponent } from "../../../Shared/searchbar/searchbar.component";
 import { Table2Component } from "../../../Shared/table2/table2.component";
+import { NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-medicamentos',
@@ -12,34 +13,36 @@ import { Table2Component } from "../../../Shared/table2/table2.component";
   templateUrl: './medicamentos.component.html',
   styleUrl: './medicamentos.component.css'
 })
-export default class MedicamentosComponent {
+export default class MedicamentosComponent implements OnInit {
 
-  medicamentos = [
-    {
-      id: 1,
-      nombreMedicina: 'Medicina A',
-      cantidad: 10,
-      categoria: 'Cancer'
-    },
-    {
-      id: 2,
-      nombreMedicina: 'Medicina B',
-      cantidad: 20,
-      categoria: 'Diabetes'
-    },
-    {
-      id: 3,
-      nombreMedicina: 'Medicina C',
-      cantidad: 30,
-      categoria: 'Hipertension'
-    }
-  ];
+  medicamentos: any[] = [];
+
+  constructor(private ngZone: NgZone) { }
+
+  ngOnInit(): void {
+    this.getMedicamentos();
+  }
+
+  getMedicamentos() {
+    fetch('http://localhost:3000/api/medication/getAll')
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json); // <--- Agrega esta línea para imprimir los datos en la consola
+        if (json && json.data && json.data.Medication && Array.isArray(json.data.Medication)) {
+          this.ngZone.run(() => {
+            this.medicamentos = json.data.Medication.slice(0, 10);
+          });
+        } else {
+          console.error('La API no devolvió un arreglo de medicamentos');
+        }
+      });
+  }
 
   editMedicamento(medicamento: any) {
-    alert(medicamento.id);
+    alert(medicamento.name);
   }
 
   deleteMedicamento(medicamento: any) {
-    alert(medicamento.id);
+    alert(medicamento.name);
   }
 }
