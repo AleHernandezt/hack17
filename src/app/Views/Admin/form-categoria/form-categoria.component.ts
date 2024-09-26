f9e5a12b6f1f1219688c9019e267bc5d002a8697
alehernandezt <j.alehernandezt@gmail.com>
2024-09-26 01:33:35 -0400

    added create categoria but not working yet

--- src/app/Views/Admin/form-categoria/form-categoria.component.ts
+++ src/app/Views/Admin/form-categoria/form-categoria.component.ts
@@ -1,17 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { H1Component } from '../../../Shared/h1/h1.component';
 import { CardComponent } from "../../../Shared/card/card.component";
-import { InputTextComponent } from "../../../Shared/input-text/input-text.component";
-import { BtnComponent } from "../../../Shared/btn/btn.component";
+import { NgZone } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form-categoria',
   standalone: true,
-  imports: [H1Component, CardComponent, InputTextComponent, BtnComponent],
+  imports: [H1Component, CardComponent, FormsModule],
   templateUrl: './form-categoria.component.html',
-  styleUrl: './form-categoria.component.css'
+  styleUrls: ['./form-categoria.component.css']
 })
-export default class FormCategoriaComponent {
-form: any;
+export default class FormCategoriaComponent implements OnInit {
 
+  form: any = {
+    name: '',
+    description: ''
+  };
+
+  constructor(private ngZone: NgZone) { }
+
+  ngOnInit(): void {
+  }
+
+  createCategory() {
+    fetch('http://localhost:3000/api/category/create', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(this.form)
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json); // <--- Agrega esta línea para imprimir los datos en la consola
+        this.ngZone.run(() => {
+          console.log('Categoría creada con éxito');
+          this.form = {
+            name: '',
+            description: ''
+          };
+        });
+      })
+      .catch((error) => {
+        console.error(error); // <--- Agrega esta línea para imprimir el error en la consola
+        this.ngZone.run(() => {
+          console.log('Error al crear la categoría');
+          
+        });
+      });
+  }
 }
