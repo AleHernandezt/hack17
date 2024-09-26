ee8b2d71845959b8d3a96fb63ee809951069a481
alehernandezt <j.alehernandezt@gmail.com>
2024-09-26 19:19:39 -0400

    sin mandar el id

--- src/app/Views/Admin/form-patologia/form-patologia.component.ts
+++ src/app/Views/Admin/form-patologia/form-patologia.component.ts
@@ -1,3 +1,54 @@
+// import { Component } from '@angular/core';
+// import { H1Component } from '../../../Shared/h1/h1.component';
+// import { FormsModule } from '@angular/forms';
+// import { NgZone } from '@angular/core';
+
+// @Component({
+//   selector: 'app-form-patologia',
+//   standalone: true,
+//   imports: [H1Component, FormsModule],
+//   templateUrl: './form-patologia.component.html',
+//   styleUrls: ['./form-patologia.component.css']
+// })
+// export default class FormPatologiaComponent {
+//   id = 400;
+//   patologia = {
+//     name: ''
+//   };
+
+//   constructor(private ngZone: NgZone) { }
+
+//   createPatologia() {
+//     fetch('http://localhost:3000/api/pathology/create', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify({
+//         id: this.id,
+//         name: this.patologia.name
+//       })
+//     })
+//       .then((response) => response.json())
+//       .then((json) => {
+//         console.log(json); // Log the response from the API
+//         if (json.message === 'Contact the administrator: error') {
+//           console.log('Error al crear la patología');
+//         } else {
+//           console.log('Patología creada con éxito');
+//           this.patologia = {
+//             name: ''
+//           };
+//           this.id++;
+//         }
+//       })
+//       .catch((error) => {
+//         console.error(error); // Log the error
+//         console.log('Error al crear la patología');
+//       });
+//   }
+// }
+
 import { Component } from '@angular/core';
 import { H1Component } from '../../../Shared/h1/h1.component';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +63,6 @@ import { NgZone } from '@angular/core';
 })
 export default class FormPatologiaComponent {
   patologia = {
-    id: 101,
     name: ''
   };
 
@@ -34,7 +84,6 @@ export default class FormPatologiaComponent {
         } else {
           console.log('Patología creada con éxito');
           this.patologia = {
-            id: this.patologia.id + 1,
             name: ''
           };
         }
