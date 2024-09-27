69c8a5fc0a0d23cc0fbb98098aebcfca669dd4fd
Cesar Martinez <cmmh200427@gmail.com>
2024-09-27 12:32:21 -0400

    arreglado fetch de perfil-paciente y gestion-donante

--- src/app/Views/Admin/gestion-donante/gestion-donante.component.ts
+++ src/app/Views/Admin/gestion-donante/gestion-donante.component.ts
@@ -1,30 +1,41 @@
 import { Component, OnInit, NgZone } from '@angular/core';
-import { BtnComponent } from "../../../Shared/btn/btn.component";
-import { TableComponent } from "../../../Shared/table/table.component";
-import { Table2Component } from "../../../Shared/table2/table2.component";
+import { BtnComponent } from '../../../Shared/btn/btn.component';
+import { TableComponent } from '../../../Shared/table/table.component';
+import { Table2Component } from '../../../Shared/table2/table2.component';
 import { H1Component } from '../../../Shared/h1/h1.component';
-import { SearchbarComponent } from "../../../Shared/searchbar/searchbar.component";
+import { SearchbarComponent } from '../../../Shared/searchbar/searchbar.component';
+import { appSettings } from '../../../settings/appsettings';
+import { getCookieHeader } from '../../../custom/getCookieHeader';
 
 @Component({
   selector: 'app-gestion-donante',
   standalone: true,
-  imports: [H1Component, BtnComponent, TableComponent, Table2Component, SearchbarComponent],
+  imports: [
+    H1Component,
+    BtnComponent,
+    TableComponent,
+    Table2Component,
+    SearchbarComponent,
+  ],
   templateUrl: './gestion-donante.component.html',
-  styleUrls: ['./gestion-donante.component.css']
+  styleUrls: ['./gestion-donante.component.css'],
 })
 export default class GestionDonanteComponent implements OnInit {
-
   donantes: any[] = [];
 
-  constructor(private ngZone: NgZone) { }
+  constructor(private ngZone: NgZone) {}
 
   ngOnInit(): void {
-    console.log("holi");
+    console.log('holi');
     this.getDonantes();
   }
 
   getDonantes() {
-    fetch('http://localhost:3000/api/charity/getAll')
+    const { headers } = getCookieHeader();
+    fetch(`${appSettings.apiUrl}charity/getAll`, {
+      method: 'GET',
+      headers: headers,
+    })
       .then((response) => response.json())
       .then((json) => {
         console.log(json); // <--- Agrega esta línea para imprimir los datos en la consola
