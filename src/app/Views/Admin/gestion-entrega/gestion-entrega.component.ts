8e8deedd1b02b734db93fb9404882e553100b7a6
alehernandezt <j.alehernandezt@gmail.com>
2024-10-01 18:00:43 -0400

    fixed url del fetch, ya hace fetch :)

--- src/app/Views/Admin/gestion-entrega/gestion-entrega.component.ts
+++ src/app/Views/Admin/gestion-entrega/gestion-entrega.component.ts
@@ -3,7 +3,8 @@ import { H1Component } from "../../../Shared/h1/h1.component";
 import { Table2Component } from '../../../Shared/table2/table2.component';
 import { SearchbarComponent } from '../../../Shared/searchbar/searchbar.component';
 import { BtnComponent } from '../../../Shared/btn/btn.component';
-import { HttpClient } from '@angular/common/http';
+import { getCookieHeader } from '../../../custom/getCookieHeader';
+import { appSettings } from '../../../settings/appsettings';
 
 @Component({
   selector: 'app-gestion-entrega',
@@ -17,17 +18,22 @@ export default class GestionEntregaComponent implements OnInit {
   columnas: string[] = ['id', 'appointment_date', 'withdrawal_date', 'treatment_id', 'patient_id', 'expiration_date', 'status'];
   encabezados: string[] = ['ID', 'Fecha de Entrega', 'Fecha de Retiro', 'ID de Tratamiento', 'ID de Paciente', 'Fecha de Expiración', 'Estado'];
 
-  constructor(private http: HttpClient, private ngZone: NgZone) { }
+  constructor(private ngZone: NgZone) { }
 
   ngOnInit(): void {
     this.getEntregas();
   }
 
   getEntregas() {
-    this.http.get('localhost:3000/api/delivery/getPending')
-      .subscribe((response: any) => {
+    const { headers } = getCookieHeader();
+    fetch(`${appSettings.apiUrl}delivery/getPending`, {
+      method: 'GET',
+      headers: headers,
+    })
+      .then((response) => response.json())
+      .then((json) => {
         this.ngZone.run(() => {
-          this.entregas = response.data.Delivery.map((entrega: any) => ({
+          this.entregas = json.data.Delivery.map((entrega: any) => ({
             id: entrega.id,
             appointment_date: entrega.appointment_date,
             withdrawal_date: entrega.withdrawal_date,
