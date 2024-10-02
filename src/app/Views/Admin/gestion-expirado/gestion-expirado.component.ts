e2bc3bd4241353d5cb5b928d9c6d5e6a39c83cb7
alehernandezt <j.alehernandezt@gmail.com>
2024-10-01 21:09:34 -0400

    added entregas expiradas, not working yet

---
+++ src/app/Views/Admin/gestion-expirado/gestion-expirado.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-expirado',
+  standalone: true,
+  imports: [],
+  templateUrl: './gestion-expirado.component.html',
+  styleUrl: './gestion-expirado.component.css'
+})
+export default class GestionExpiradoComponent {
+
+}
